fb4bb9541c97a029d09e7875c8be3c930003bfa4
Carl Mate <cpmate@up.edu.ph>
2022-01-02 12:17:09 +0800

    Redesign ui

--- index.php
+++ index.php
@@ -6,9 +6,103 @@
 	}
 ?>
 
-<h1>Find and connect with a tutor!</h1>
+<div class="sidebar">
+    <div class="logo-details">
+        <i class='bx bxl-c-plus-plus'></i>
+        <span class="logo_name">UP Peer Tutoring</span>
+    </div>
+    <ul class="nav-links">
+        <li>
+            <a href="#">
+                <i class='bx bx-grid-alt' ></i>
+                <span class="link_name">Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-box'></i>
+                <span class="link_name">Product</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-list-ul' ></i>
+                <span class="link_name">Order List</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-pie-chart-alt-2' ></i>
+                <span class="link_name">Analytics</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-coin-stack' ></i>
+                <span class="link_name">Stock</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-user' ></i>
+                <span class="link_name">Team</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-message' ></i>
+                <span class="link_name">Message</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-heart' ></i>
+                <span class="link_name">Favorites</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-cog' ></i>
+                <span class="link_name">Settings</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-log-out' ></i>
+                <span class="link_name">Log out</span>
+            </a>
+        </li>
+    </ul>
+</div>
+
+<section class="home-section">
+    <nav>
+        <div class="sidebar-button">
+           <i class='bx bx-menu sidebarBtn' ></i> 
+            <span class="dashboard">Dashboard</span>
+        </div>
+        <div class="profile-details">
+            <img src="images/user.png" alt="">
+            <span class="user">Admin</span>
+           <i class='bx bx-chevron-down' ></i> 
+        </div>
+    </nav>
+
+    <div class="home-content">
+        <div class="overview-boxes">
+            <div class="box">
+                <div class="left-side">
+                    <div class="box-topic">Welcome, Admin! Do something with your admin skills!</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="script.js"></script>
+<!--
 <div class="content">
-			<?php if(isset($_SESSION['success'])): ?>
+//			<?php if(isset($_SESSION['success'])): ?>
 				<div class="error success">
 					<h4>
 						<?php 
@@ -30,6 +124,7 @@
                     </div>
                 </div>
             <?php endif ?>
-        </div>
+</div>
+-->
 </body>
 </html>
